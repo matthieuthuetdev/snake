4516f4a3f3e6c7f0bc04f11f346dd46ebf3364ff
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-07-17 16:35:14 +0200

    passage du fichier js principale a des méthode

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,9 @@
-import { serpent } from "./serpent.js";
+import { Tableau } from "./Tableau.js";
+import { Serpent } from "./serpent.js";
 const start = document.getElementById("start");
 start.addEventListener("click", () => {
+const table = new Tableau(20,20)
+table.cree();
+table.afficher();
 
 });
