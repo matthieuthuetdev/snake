ce70129ddae4799fde1799fcb018583164dddfcd
matthieuthuetdev <mthuet.pro@gmail.com>
2024-07-17 07:55:57 +0200

    création de la transition après l'appuis sur le bouton

--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,13 @@
 import { genererTableau } from "./function.js";
-genererTableau(10,10)
+const h = window.innerheight;
+const l = window.innerWidth;
+const start = document.getElementById("start");
+start.addEventListener("click", () => {
+  genererTableau(20, 20);
+  const table = document.getElementById("table");
+  table.classList.remove("display_none");
+  table.style.height = h / 2 + "px";
+  table.style.width = h + "px"
+  const befor_start = document.getElementById("beforstart");
+  befor_start.style.display = "none";
+});
