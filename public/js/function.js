e854a82f00f31a15786e164586e5d1336c095fd3
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-07-17 11:49:34 +0200

    séparation du code dans une fonction

--- public/js/function.js
+++ public/js/function.js
@@ -4,8 +4,28 @@ function genererTableau(l, c) {
         const ligne = table.insertRow()
         for (let i_c = 0; i_c < c; i_c++) {
             const cases = ligne.insertCell()
-            cases.setAttribute("id","l"+i_l+"c"+i_c)
+            cases.setAttribute("id", "l" + i_l + "c" + i_c)
         }
     }
+}
+function afficherTableau() {
+    const h = window.innerHeight;
+    const l = window.innerWidth;
+    const table = document.getElementById("table");
+    table.classList.remove("display_none");
+    table.style.height = h * 0.9 + "px"
+    table.style.width = h * 0.9 + "px"
+    table.style.position = "absolute"
+    table.style.left = "25%"
+    table.style.top = "10%"
+    const l5c6 = document.getElementById("l10c10")
+    const befor_start = document.getElementById("beforstart");
+    befor_start.style.display = "none";
+
+}
+function mouvement(){
+    let teteLigne = 10
+    let teteColone = 10
     
-}export {genererTableau}
+}
+export { genererTableau, afficherTableau }
--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,7 @@
-import { genererTableau } from "./function.js";
-const h = window.innerheight;
-const l = window.innerWidth;
+import { genererTableau, afficherTableau} from "./function.js";
 const start = document.getElementById("start");
 start.addEventListener("click", () => {
   genererTableau(20, 20);
-  const table = document.getElementById("table");
-  table.classList.remove("display_none");
-  table.style.height = h / 2 + "px";
-  table.style.width = h + "px"
-  const befor_start = document.getElementById("beforstart");
-  befor_start.style.display = "none";
+  afficherTableau()
+  let direction
 });
