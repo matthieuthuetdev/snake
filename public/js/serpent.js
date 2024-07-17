0180f3a2820777af77a9c22c614c31149c40391b
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-07-17 15:14:58 +0200

    création du construceur de la classe serpent

--- public/js/serpent.js
+++ public/js/serpent.js
@@ -1,3 +1,9 @@
 class Serpent{
-    
+    serpent(teteLigne, teteColone, direction, longueure, scor){
+        this.teteLigne = teteLigne;
+        this.teteColone = teteColone;
+        this.direction = direction;
+        this.longueure = longueure;
+        this.scor = scor;
+    }
 }
