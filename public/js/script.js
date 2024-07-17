86538a7ebd8ecc124d10bde2a36485524d8b90c7
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-07-17 15:41:49 +0200

    création du constructeur de la fonction serpent

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,5 @@
-import { genererTableau, afficherTableau } from "./function.js";
+import { serpent } from "./serpent.js";
 const start = document.getElementById("start");
 start.addEventListener("click", () => {
-  genererTableau()
-  let teteLigne = 10
-  let teteColone = 10
-  let longueure = 3
-  let direction = changeDirection()
-  
 
 });
