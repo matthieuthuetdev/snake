85ed2b04ed58fe34f9ef5522f7aa8b2a35c7815d
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-07-18 10:10:47 +0200

    retrai du alerte qui montre la touche saisie

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,11 @@
 import { Tableau } from "./Tableau.js";
-import { Serpent } from "./serpent.js";
+import { Serpent } from "./Serpent.js";
 const start = document.getElementById("start");
+
 start.addEventListener("click", () => {
-const table = new Tableau(20,20)
-const serpent = new Serpent()
-serpent.mouvement()
-table.cree();
-table.afficher();
-
-
+    const table = new Tableau(20, 20)
+    const serpent = new Serpent()
+    table.cree();
+    table.afficher();
+    serpent.mouvement()
 });
