1252a3b0a7f0356d68683276239ee08736398a19
matthieuthuetdev <mthuet.pro@gmail.com>
2024-07-18 07:46:27 +0200

    création de la fonction de mouvement du serpent

--- public/js/script.js
+++ public/js/script.js
@@ -3,7 +3,10 @@ import { Serpent } from "./serpent.js";
 const start = document.getElementById("start");
 start.addEventListener("click", () => {
 const table = new Tableau(20,20)
+const serpent = new Serpent()
+serpent.mouvement()
 table.cree();
 table.afficher();
+
 
 });
--- public/js/serpent.js
+++ public/js/serpent.js
@@ -1,10 +1,41 @@
 class Serpent {
-    serpent(teteLigne, teteColone, direction, longueure, scor) {
-        this.teteLigne = teteLigne;
-        this.teteColone = teteColone;
-        this.direction = direction;
-        this.longueure = longueure;
-        this.scor = scor;
-    }
+  serpent() {
+    this.teteLigne = 10;
+    this.teteColone = 10;
+    this.direction = 0;
+    this.longueure = 3;
+    this.scor = 0;
+  }
+  mouvement() {
+    document.addEventListener("keydown", (e) => {
+      if (e.key == "arrowUp") {
+        this.direction = "up";
+      } else if (e.key == "arrowRight") {
+        this.direction = "right";
+      } else if (e.key == "arrowDown") {
+        this.direction = "down";
+      } else if (e.key == "arrowLeft") {
+        this.direction = "left";
+      }
+    });
+    setInterval(() => {
+      if (this.direction == "up") {
+        this.teteLigne++;
+      } else if (this.direction == "right") {
+        this.teteLigne++;
+      } else if (this.direction == "down") {
+        this.teteLigne--;
+      } else if (this.direction == "left") {
+        this.teteColone--;
+      }
+      const caseSible = document.getElementById("l"+this.teteLigne + "c"+ this.teteColone)
+      caseSible.setAttribute("class","snake")
+      
+    }, 500);
+  }
+
+  jouer() {
+    this.mouvement();
+  }
 }
-export { Serpent }
+export { Serpent };
